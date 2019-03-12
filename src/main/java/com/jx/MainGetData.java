ac1f225b9e0ef3f92275c27805f07297d9dc2286
Jiangxu <634234872@qq.com>
2019-03-12 18:53:20 +0800

    更新数据为2019年1月中华人民共和国县以上行政区划代码

--- src/main/java/com/jx/MainGetData.java
+++ src/main/java/com/jx/MainGetData.java
@@ -16,8 +16,8 @@ public class MainGetData {
     public static void main(String[] args) {
         try {
             //2018年11月中华人民共和国县以上行政区划代码网页
-            Document doc = Jsoup.connect("http://www.mca.gov.cn/article/sj/xzqh/2018/201804-12/20181101021046.html").maxBodySize(0).get();
-            Elements elements = doc.getElementsByClass("xl7024197");
+            Document doc = Jsoup.connect("http://www.mca.gov.cn/article/sj/xzqh/2019/201901-06/201902061009.html").maxBodySize(0).get();
+            Elements elements = doc.getElementsByClass("xl7016597");
             List<String> stringList = elements.eachText();
             List<String> stringName = new ArrayList<String>();
             List<String> stringCode = new ArrayList<String>();
@@ -36,7 +36,7 @@ public class MainGetData {
                 throw new RuntimeException("数据错误");
             }
             List<Province> provinceList = processData(stringName, stringCode);
-            String path = FileUtils.getProjectDir() + "/2018年11月中华人民共和国县以上行政区划代码" + ".json";
+            String path = FileUtils.getProjectDir() + "/2019年1月中华人民共和国县以上行政区划代码" + ".json";
             JSONFormatUtils.jsonWriter(provinceList, path);
         } catch (IOException e) {
             e.printStackTrace();
